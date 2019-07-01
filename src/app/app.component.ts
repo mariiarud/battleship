44c806a1a5b2ecad02d72a467cefbd3f2c4b1222
InterLink Intern <intern@intern-pc.interlink.office>
2019-07-01 14:19:42 +0300

    make it better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import io from "socket.io-client";
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'Battleship';
+  isCall = false;
+  gameStatus = 'home';
+  playerId: string = '';
+  private socket: any;
+  rooms = [];
+  currentRoom = -1;
+  private notifier: NotifierService;
+  score = 20;
+  
+  public constructor( notifier: NotifierService ) {
+		this.notifier = notifier;
+	}
+
+  ngOnInit(): void {
+    this.socket = io("http://localhost:3200");
+  }
+
+  ngAfterViewInit(): void {
+    this.socket.on("callReset", ()=>{
+      this.isCall=false;
+    });
+    this.socket.on("roomStatus", data =>{
+      this.rooms = data;
+    });
+    this.socket.on("newPlayer", (id, room) =>{
+      this.playerId = id;
+      this.gameStatus = 'new';
+      this.currentRoom = room;
+    });
+    this.socket.on("roomIsFull", () =>{
+      this.notifier.notify( "warning", "Room is occupied!" );
+    });
+    this.socket.on("rollCall", id=>{
+      if(this.playerId == '' && !this.isCall){
+        this.isCall = true;
+        this.socket.emit("iAmGuest");
+      }
+      else
+      if(this.playerId == id)
+        this.socket.emit("iAmHere", this.playerId, this.currentRoom);
+    })
+    this.socket.on("startNewGame", ()=>{
+      this.gameStatus = 'start';
+    });
+  }
+
+  joinRoom(i): void {
+    this.socket.emit("joinRoom", i);
+    
+  }
 }
