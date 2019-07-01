6c9f26afc4b418c2b3259b58e0b449dfcd504e26
Neliry <mariyars11@gmail.com>
2019-07-01 23:47:48 +0300

    make it better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,6 +57,16 @@ export class AppComponent {
     this.socket.on("playerLeave", room=>{
       if(this.currentRoom == room){
         this.notifier.notify( "error", "Opponent left the game!" );
+        if(this.gameStatus == "start"){
+          let timeLeft: number = 2;
+          let interval = setInterval(() => {
+            if(timeLeft > 0) {
+              timeLeft--;
+            } else {
+              this.gameStatus = "home";
+            }
+          },1000);
+        }
       }
     });
   }
